5fcd24ec0e8b5467802da7413f4670e697fd448d
sobbey99 <sobbey99@gmail.com>
2022-02-08 14:38:13 +0200

    Finished First Page

--- src/js/script.js
+++ src/js/script.js
@@ -48,6 +48,7 @@ function moveBackground() {
     'transform': translate
   });
 
+
   window.requestAnimationFrame(moveBackground);
 }
 
@@ -72,7 +73,7 @@ new fullpage('#fullpage', {
 	autoScrolling:true,
 	scrollHorizontally: false,
   navigation: true,
-  navigationTooltips: ['Главная', 'Что такое рекуператор?', 'Преимущества', 'Выгода', 'Каталог'],
+  navigationTooltips: ['Главная', 'Что такое рекуператор?', 'Преимущества', 'Выгода', 'Каталог', 'Монтаж', 'Связаться'],
 	showActiveTooltip: false,
   v2compatible: true
 });
@@ -272,6 +273,53 @@ observerAdvantage.observe(targetAdvantage,  configAdvantage);
 // EDGE WATCHING ! выбираем нужный элемент
 const targetEdge = document.querySelector('.edge');
 const edgeItem = document.querySelector('.edge__wrapper');
+//SVG 1
+const edgeImg1 = document.querySelector('.edge-item__img_1');
+const edgeSvg1El1 = document.querySelector('.edge-svg1_el1');
+const edgeSvg1El2 = document.querySelector('.edge-svg1_el2');
+const edgeSvg1El3 = document.querySelector('.edge-svg1_el3');
+const edgeSvg1El4 = document.querySelector('.edge-svg1_el4');
+const edgeSvg1El5 = document.querySelector('.edge-svg1_el5');
+const edgeSvg1El6 = document.querySelector('.edge-svg1_el6');
+
+//SVG 2
+const edgeImg2 = document.querySelector('.edge-item__img_2');
+const edgeSvg2El1 = document.querySelector('.edge-svg2_el1');
+const edgeSvg2El2 = document.querySelector('.edge-svg2_el2');
+const edgeSvg2El3 = document.querySelector('.edge-svg2_el3');
+const edgeSvg2El4 = document.querySelector('.edge-svg2_el4');
+const edgeSvg2El5 = document.querySelector('.edge-svg2_el5');
+const edgeSvg2El6 = document.querySelector('.edge-svg2_el6');
+const edgeSvg2El7 = document.querySelector('.edge-svg2_el7');
+const edgeSvg2El8 = document.querySelector('.edge-svg2_el8');
+const edgeSvg2El9 = document.querySelector('.edge-svg2_el9');
+
+//SVG 3
+const edgeImg3 = document.querySelector('.edge-item__img_3');
+const edgeSvg3El1 = document.querySelector('.edge-svg3_el1');
+const edgeSvg3El2 = document.querySelector('.edge-svg3_el2');
+const edgeSvg3El3 = document.querySelector('.edge-svg3_el3');
+const edgeSvg3El4 = document.querySelector('.edge-svg3_el4');
+const edgeSvg3El5 = document.querySelector('.edge-svg3_el5');
+const edgeSvg3El6 = document.querySelector('.edge-svg3_el6');
+const edgeSvg3El7 = document.querySelector('.edge-svg3_el7');
+
+
+//SVG 4
+const edgeImg4 = document.querySelector('.edge-item__img_4');
+const edgeSvg4El1 = document.querySelector('.edge-svg4_el1');
+
+//SVG 5
+const edgeImg5 = document.querySelector('.edge-item__img_5');
+const edgeSvg5El1 = document.querySelector('.edge-svg5_el1');
+
+//SVG 6
+const edgeImg6 = document.querySelector('.edge-item__img_6');
+const edgeSvg6El1 = document.querySelector('.edge-svg6_el1');
+const edgeSvg6El2 = document.querySelector('.edge-svg6_el2');
+const edgeSvg6El3 = document.querySelector('.edge-svg6_el3');
+
+
 var edgeChanger = 0;
 
  
@@ -286,14 +334,116 @@ var observerEdge = new MutationObserver(function(mutations) {
             edgeItem.classList.add('animate__bounceIn');
 
 
+            setTimeout(() => {
+              //ANIMATE SVG 1
+              edgeImg1.classList.add('edge-item__img_1-activated');
+            edgeSvg1El1.classList.add('svg-elem-1');
+            edgeSvg1El2.classList.add('svg-elem-2');
+            edgeSvg1El3.classList.add('svg-elem-3');
+            edgeSvg1El4.classList.add('svg-elem-4');
+            edgeSvg1El5.classList.add('svg-elem-5');
+            edgeSvg1El6.classList.add('svg-elem-6');
+
+              //ANIMATE SVG 2
+            edgeImg2.classList.add('edge-item__img_2-activated');
+            edgeSvg2El1.classList.add('svg-elem-1');
+            edgeSvg2El2.classList.add('svg-elem-2');
+            edgeSvg2El3.classList.add('svg-elem-3');
+            edgeSvg2El4.classList.add('svg-elem-4');
+            edgeSvg2El5.classList.add('svg-elem-5');
+            edgeSvg2El6.classList.add('svg-elem-6');
+            edgeSvg2El7.classList.add('svg-elem-7');
+            edgeSvg2El8.classList.add('svg-elem-8');
+            edgeSvg2El9.classList.add('svg-elem-9');
+
+            //ANIMATE SVG 3
+            edgeImg3.classList.add('edge-item__img_3-activated');
+            edgeSvg3El1.classList.add('svg-elem-1');
+            edgeSvg3El2.classList.add('svg-elem-2');
+            edgeSvg3El3.classList.add('svg-elem-3');
+            edgeSvg3El4.classList.add('svg-elem-4');
+            edgeSvg3El5.classList.add('svg-elem-5');
+            edgeSvg3El6.classList.add('svg-elem-6');
+            edgeSvg3El7.classList.add('svg-elem-7');
+
+            //ANIMATE SVG 4
+            edgeImg4.classList.add('edge-item__img_4-activated');
+            edgeSvg4El1.classList.add('svg-elem-1');
+
+            //ANIMATE SVG 5
+            edgeImg5.classList.add('edge-item__img_5-activated');
+            edgeSvg5El1.classList.add('svg-elem-1');
+
+              //ANIMATE SVG 6
+              edgeImg6.classList.add('edge-item__img_6-activated');
+              edgeSvg6El1.classList.add('svg-elem-1');
+              edgeSvg6El2.classList.add('svg-elem-2');
+              edgeSvg6El3.classList.add('svg-elem-3');
+
+            
+
+            }, 1100);
+            break;
+            
+
+
+
          
-          break;
+          
         }
 
         if (i !== "active") {
           edgeItem.classList.remove('animate__animated');
             edgeItem.classList.remove('animate__delay-1s');
             edgeItem.classList.remove('animate__bounceIn');
+
+            setTimeout( () => {
+              //SVG 1
+            edgeImg1.classList.remove('edge-item__img_1-activated');
+            edgeSvg1El1.classList.remove('svg-elem-1');
+            edgeSvg1El2.classList.remove('svg-elem-2');
+            edgeSvg1El3.classList.remove('svg-elem-3');
+            edgeSvg1El4.classList.remove('svg-elem-4');
+            edgeSvg1El5.classList.remove('svg-elem-5');
+            edgeSvg1El6.classList.remove('svg-elem-6');
+
+            //SVG 2
+            edgeImg2.classList.remove('edge-item__img_2-activated');
+            edgeSvg2El1.classList.remove('svg-elem-1');
+            edgeSvg2El2.classList.remove('svg-elem-2');
+            edgeSvg2El3.classList.remove('svg-elem-3');
+            edgeSvg2El4.classList.remove('svg-elem-4');
+            edgeSvg2El5.classList.remove('svg-elem-5');
+            edgeSvg2El6.classList.remove('svg-elem-6');
+            edgeSvg2El7.classList.remove('svg-elem-7');
+            edgeSvg2El8.classList.remove('svg-elem-8');
+            edgeSvg2El9.classList.remove('svg-elem-9');
+
+            //SVG 3
+            edgeImg3.classList.remove('edge-item__img_3-activated');
+            edgeSvg3El1.classList.remove('svg-elem-1');
+            edgeSvg3El2.classList.remove('svg-elem-2');
+            edgeSvg3El3.classList.remove('svg-elem-3');
+            edgeSvg3El4.classList.remove('svg-elem-4');
+            edgeSvg3El5.classList.remove('svg-elem-5');
+            edgeSvg3El6.classList.remove('svg-elem-6');
+            edgeSvg3El7.classList.remove('svg-elem-7');
+
+            //SVG 4
+            edgeImg4.classList.remove('edge-item__img_4-activated');
+            edgeSvg4El1.classList.remove('svg-elem-1');
+
+ //SVG 5
+ edgeImg5.classList.remove('edge-item__img_5-activated');
+ edgeSvg5El1.classList.remove('svg-elem-1');
+
+   //SVG 6
+   edgeImg6.classList.remove('edge-item__img_6-activated');
+   edgeSvg6El1.classList.remove('svg-elem-1');
+   edgeSvg6El2.classList.remove('svg-elem-2');
+   edgeSvg6El3.classList.remove('svg-elem-3');
+
+            }, 1000)
         }
     }
   });    
@@ -314,4 +464,5 @@ observerEdge.observe(targetEdge,  configEdge);
   max: 30,
   speed: 700
 });
+
  
