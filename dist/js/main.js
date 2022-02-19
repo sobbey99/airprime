a9d9d03999c5bdea8d0fe8c5ddb67d864be4b3d1
sobbey99 <sobbey99@gmail.com>
2022-02-19 13:02:15 +0200

    Redesigned Tablet version, Finished 776-575 Media, redesign mobile menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,9 @@
 
 
 
-
+window.addEventListener(`resize`, () => {
+  location.reload()
+}, false);
 
 
 /*
@@ -40,7 +42,7 @@ $(".owl-carousel").owlCarousel({
       const edgeMobile2 = document.querySelector('.edge-mobile_2');
 
       function myEscape() {
-        if($(window).width() < 777)
+        if($(window).width() < 992)
         {   
             edgePC.classList.remove('section');
             escapeSection.classList.add('section');
@@ -352,6 +354,11 @@ observerAdvantage.observe(targetAdvantage,  configAdvantage);
 const hamburgerElement = document.querySelector('.hamburger');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const hamburgerClose = document.querySelector('.hamburger-menu_close');
+const hamburgerBottom = document.querySelector('.hamburger-menu__bottom');
+const hamburgerRights = document.querySelector('.hamburger-menu__copyright');
+const hamburgerTop = document.querySelector('.hamburger-menu__top');
+const menutag = document.querySelector('.menutag');
+
 const navFullPage = document.querySelector('#fp-nav.fp-right');
 const navList = document.querySelectorAll('.hamburger-menu nav.list ul li');
 
@@ -359,11 +366,62 @@ const navList = document.querySelectorAll('.hamburger-menu nav.list ul li');
 hamburgerElement.addEventListener('click', function(){
   hamburgerMenu.classList.add('hamburger-menu_active');
   navFullPage.classList.add('fp-right_none');
-  navList.forEach(
-    function(ele) {
-      ele.classList.add('animate__fadeInUp');
-    }
-  );
+  menutag.classList.add('liOnFlex');
+  menutag.classList.add('animate__animated');
+  menutag.classList.add('animate__slideInLeft');
+
+  hamburgerTop.classList.add('liOnFlex');
+  hamburgerTop.classList.add('animate__animated');
+  hamburgerTop.classList.add('animate__pulse');
+  hamburgerTop.classList.add('animate__fast');
+  hamburgerTop.classList.add('animate__infinite');
+
+
+
+  hamburgerClose.classList.add('liOn');
+  hamburgerClose.classList.add('animate__animated');
+  hamburgerClose.classList.add('animate__delay-4s');
+  hamburgerClose.classList.add('animate__zoomIn');
+  hamburgerClose.classList.add('animate__faster');
+
+
+
+
+
+
+
+
+  setTimeout(()=>{
+    navList.forEach(
+      function(ele, index) {
+let animateDelay = 'animate__delay-';
+animateDelay = 'animate__delay-' + index + "s";
+        ele.classList.add(animateDelay);
+        ele.classList.add('animate__fadeInUp');
+        ele.classList.add('animate__faster');
+        ele.classList.add('liOn');
+      }
+    );
+
+    hamburgerBottom.classList.add('liOnGrid');
+  hamburgerBottom.classList.add('animate__animated');
+  hamburgerBottom.classList.add('animate__fadeInUpBig');
+
+  
+
+  hamburgerRights.classList.add('liOn');
+  hamburgerRights.classList.add('animate__animated');
+  hamburgerRights.classList.add('animate__delay-3s');
+  hamburgerRights.classList.add('animate__faster');
+  hamburgerRights.classList.add('animate__slideInLeft');
+
+  
+  }, 600);
+
+
+
+
+  
   
 
 });
@@ -371,7 +429,44 @@ hamburgerElement.addEventListener('click', function(){
 hamburgerClose.addEventListener('click', function(){
   hamburgerMenu.classList.remove('hamburger-menu_active');
   navFullPage.classList.remove('fp-right_none');
+  navList.forEach(
+    function(ele) {
+      ele.classList.remove('liOn');
+      ele.classList.remove('animate__fadeInUp');
+      
 
+    }
+  );
+  
+  hamburgerBottom.classList.remove('liOnGrid');
+  hamburgerBottom.classList.remove('animate__animated');
+  hamburgerBottom.classList.remove('animate__fadeInUpBig');
+
+  hamburgerRights.classList.remove('liOn');
+  hamburgerRights.classList.remove('animate__animated');
+  hamburgerRights.classList.remove('animate__delay-3s');
+  hamburgerRights.classList.remove('animate__faster');
+  hamburgerRights.classList.remove('animate__slideInLeft');
+
+
+
+  menutag.classList.remove('liOnFlex');
+  menutag.classList.remove('animate__animated');
+  menutag.classList.remove('animate__slideInLeft');
+
+  hamburgerTop.classList.remove('liOnFlex');
+  hamburgerTop.classList.remove('animate__animated');
+  hamburgerTop.classList.remove('animate__pulse');
+  hamburgerTop.classList.remove('animate__fast');
+  hamburgerTop.classList.remove('animate__infinite');
+
+
+
+  hamburgerClose.classList.remove('liOn');
+  hamburgerClose.classList.remove('animate__animated');
+  hamburgerClose.classList.remove('animate__delay-4s');
+  hamburgerClose.classList.remove('animate__bounceIn');
+  hamburgerClose.classList.remove('animate__faster');
 });
 
 
